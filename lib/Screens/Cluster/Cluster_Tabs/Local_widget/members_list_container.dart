0b75a3d90378cd552998da71d6367050def8543c
Destiny-Ed <dikeachaeze@gmail.com>
2022-05-03 21:45:34 +0100

    Added toggle visibility switcher for members list container

---
+++ lib/Screens/Cluster/Cluster_Tabs/Local_widget/members_list_container.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:moni_app/Styles/color.dart';
+import 'package:moni_app/Styles/dimens.dart';
+
+class MembersListContainer extends StatefulWidget {
+  const MembersListContainer({Key? key}) : super(key: key);
+
+  @override
+  State<MembersListContainer> createState() => _MembersListContainerState();
+}
+
+class _MembersListContainerState extends State<MembersListContainer> {
+  bool _isShow = true;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 15,
+          ),
+
+          ///Header
+          Container(
+            // margin: const EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Overdue Loans",
+                  style: TextStyle(
+                      fontSize: AppSize.listTitle, color: AppColor.black),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _isShow = !_isShow;
+                    });
+                  },
+                  child: Icon(
+                    _isShow == true ? Icons.minimize : Icons.add,
+                    color: AppColor.grey,
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          const Divider(),
+          if (_isShow == true)
+            ListTile(
+              contentPadding: const EdgeInsets.all(0),
+              // contentPadding: const EdgeInsets.only(top: 10),
+              leading: Container(
+                height: 45,
+                width: 45,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: const DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/avatar.png"),
+                    )),
+              ),
+              title: Row(
+                children: [
+                  Text(
+                    "Florence Tanika",
+                    style: TextStyle(
+                        color: AppColor.black, fontSize: AppSize.listTitle),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Icon(
+                      Icons.lens,
+                      size: 4,
+                      color: AppColor.grey,
+                    ),
+                  ),
+                  Text(
+                    '3 days over due',
+                    style: TextStyle(
+                        color: AppColor.red, fontSize: AppSize.smallText),
+                  ),
+                ],
+              ),
+              subtitle: Text(
+                '₦10,555,000 Late loan ',
+                style: TextStyle(
+                    fontSize: AppSize.smallText,
+                    color: AppColor.red,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          if (_isShow == true) const Divider(),
+        ],
+      ),
+    );
+  }
+}
--- lib/Screens/Cluster/Cluster_Tabs/members_page.dart
+++ lib/Screens/Cluster/Cluster_Tabs/members_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moni_app/Screens/Cluster/Cluster_Tabs/Local_widget/members_list_container.dart';
 import 'package:moni_app/Styles/color.dart';
 import 'package:moni_app/Styles/dimens.dart';
 
@@ -16,87 +17,7 @@ class _MembersPageState extends State<MembersPage> {
       physics: const NeverScrollableScrollPhysics(),
       children: [
         ...List.generate(1000, (index) {
-          return Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 15,
-                ),
-
-                ///Header
-                Container(
-                  // margin: const EdgeInsets.symmetric(vertical: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Overdue Loans",
-                        style: TextStyle(
-                            fontSize: AppSize.listTitle, color: AppColor.black),
-                      ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Icon(
-                          Icons.minimize,
-                          color: AppColor.grey,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Divider(),
-                ListTile(
-                  contentPadding: const EdgeInsets.all(0),
-                  // contentPadding: const EdgeInsets.only(top: 10),
-                  leading: Container(
-                    height: 45,
-                    width: 45,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: const DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage("assets/avatar.png"),
-                        )),
-                  ),
-                  title: Row(
-                    children: [
-                      Text(
-                        "Florence Tanika",
-                        style: TextStyle(
-                            color: AppColor.black, fontSize: AppSize.listTitle),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8),
-                        child: Icon(
-                          Icons.lens,
-                          size: 4,
-                          color: AppColor.grey,
-                        ),
-                      ),
-                      Text(
-                        '3 days over due',
-                        style: TextStyle(
-                            color: AppColor.red, fontSize: AppSize.smallText),
-                      ),
-                    ],
-                  ),
-                  subtitle: Text(
-                    '₦10,555,000 Late loan ',
-                    style: TextStyle(
-                        fontSize: AppSize.smallText,
-                        color: AppColor.red,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                const Divider(),
-              ],
-            ),
-          );
+          return const MembersListContainer();
         }),
       ],
     );
