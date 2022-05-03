c8a1e58743666cdf4d654c87884e938b1d536c30
Destiny-Ed <dikeachaeze@gmail.com>
2022-05-04 00:21:13 +0100

    Display all member(agents) loan details

--- lib/Screens/Cluster/Cluster_Tabs/Local_widget/members_list_container.dart
+++ lib/Screens/Cluster/Cluster_Tabs/Local_widget/members_list_container.dart
@@ -4,12 +4,17 @@ import 'package:moni_app/Styles/color.dart';
 import 'package:moni_app/Styles/dimens.dart';
 
 class MembersListContainer extends StatefulWidget {
-  const MembersListContainer({Key? key, this.title, this.agentsList})
+  const MembersListContainer(
+      {Key? key, this.color, this.title, this.loanType, this.agentsList})
       : super(key: key);
 
   final List<ActiveAgent>? agentsList;
 
   final String? title;
+
+  final Color? color;
+
+  final String? loanType;
 
   @override
   State<MembersListContainer> createState() => _MembersListContainerState();
@@ -57,50 +62,61 @@ class _MembersListContainerState extends State<MembersListContainer> {
           ),
           const Divider(),
           if (_isShow == true)
-            ListTile(
-              contentPadding: const EdgeInsets.all(0),
-              // contentPadding: const EdgeInsets.only(top: 10),
-              leading: Container(
-                height: 45,
-                width: 45,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: const DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage("assets/avatar.png"),
-                    )),
-              ),
-              title: Row(
+            ...List.generate(widget.agentsList!.length, (index) {
+              final agent = widget.agentsList![index];
+
+              return Column(
                 children: [
-                  Text(
-                    "Florence Tanika",
-                    style: TextStyle(
-                        color: AppColor.black, fontSize: AppSize.listTitle),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Icon(
-                      Icons.lens,
-                      size: 4,
-                      color: AppColor.grey,
+                  ListTile(
+                    contentPadding: const EdgeInsets.all(0),
+                    // contentPadding: const EdgeInsets.only(top: 10),
+                    leading: Container(
+                      height: 45,
+                      width: 45,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage("assets/avatar.png"),
+                          )),
+                    ),
+                    title: Row(
+                      children: [
+                        Text(
+                          "${agent.agent!.firstName} ${agent.agent!.lastName}",
+                          style: TextStyle(
+                              color: AppColor.black,
+                              fontSize: AppSize.listTitle),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: Icon(
+                            Icons.lens,
+                            size: 4,
+                            color: AppColor.grey,
+                          ),
+                        ),
+                        Text(
+                          '3 days over due',
+                          style: TextStyle(
+                              color: widget.color, fontSize: AppSize.smallText),
+                        ),
+                      ],
+                    ),
+                    subtitle: Text(
+                      agent.agent!.recentLoan == null
+                          ? widget.loanType!
+                          : '₦${agent.agent!.recentLoan!.loanAmount!} Active loan ',
+                      style: TextStyle(
+                          fontSize: AppSize.smallText,
+                          color: widget.color,
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
-                  Text(
-                    '3 days over due',
-                    style: TextStyle(
-                        color: AppColor.red, fontSize: AppSize.smallText),
-                  ),
+                  if (_isShow == true) const Divider(),
                 ],
-              ),
-              subtitle: Text(
-                '₦10,555,000 Late loan ',
-                style: TextStyle(
-                    fontSize: AppSize.smallText,
-                    color: AppColor.red,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-          if (_isShow == true) const Divider(),
+              );
+            })
         ],
       ),
     );
--- lib/Screens/Cluster/Cluster_Tabs/members_page.dart
+++ lib/Screens/Cluster/Cluster_Tabs/members_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:moni_app/Models/member_model.dart';
 import 'package:moni_app/Screens/Cluster/Cluster_Tabs/Local_widget/members_list_container.dart';
+import 'package:moni_app/Styles/color.dart';
 
 class MembersPage extends StatefulWidget {
   const MembersPage({Key? key, this.data}) : super(key: key);
@@ -21,9 +22,10 @@ class _MembersPageState extends State<MembersPage> {
         ///Overdue loans agent
         if (agentList.overdueAgents!.isNotEmpty)
           MembersListContainer(
-            title: 'Overdue Loans',
-            agentsList: agentList.overdueAgents,
-          ),
+              title: 'Overdue Loans',
+              agentsList: agentList.overdueAgents,
+              loanType: 'Late loan',
+              color: AppColor.red),
 
         ///Due Today agents
 
@@ -31,20 +33,26 @@ class _MembersPageState extends State<MembersPage> {
           MembersListContainer(
             title: 'Due Today',
             agentsList: agentList.dueAgents,
+            loanType: 'Late loan',
+            color: AppColor.darkAmber,
           ),
 
         ///Active Loan agents
         if (agentList.activeAgents!.isNotEmpty)
           MembersListContainer(
             title: 'Active Loans',
+            loanType: 'Active loan',
             agentsList: agentList.activeAgents,
+            color: AppColor.green,
           ),
 
         ///Inactive Loan agents
         if (agentList.inactiveAgents!.isNotEmpty)
           MembersListContainer(
             title: 'Inactive Loans',
+            loanType: 'No Active loan',
             agentsList: agentList.inactiveAgents,
+            color: AppColor.black,
           ),
       ],
     );
