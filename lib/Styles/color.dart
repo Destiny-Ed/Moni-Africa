9bc914fe3ecf3ead695b8b19425c66f2c8bc4a98
Destiny-Ed <dikeachaeze@gmail.com>
2022-05-03 16:23:00 +0100

    Structure project and add some styles and dimens

---
+++ lib/Styles/color.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color pink = const Color(0xffE66652);
+
+  static Color white = const Color(0xffFFFFFF);
+
+  static Color green = const Color(0xff7DDE86);
+
+  static Color lightAmber = const Color(0xffF0CC79);
+
+  static Color darkAmber = const Color(0xffEAB948);
+
+  static Color grey = const Color(0xffCDCFD0);
+
+  static Color red = const Color(0xffE41002);
+
+  static Color black = const Color(0xff13163E);
+
+  static Color textBlack = const Color(0xff4F6482);
+}
---
+++ lib/Styles/dimens.dart
@@ -0,0 +1,11 @@
+class AppSize {
+  static double smallestText = 9.0;
+
+  static double smallText = 12.0;
+
+  static double listTitle = 13.16;
+
+  static double mediumText = 14.0;
+
+  static double bigText = 17.0;
+}
